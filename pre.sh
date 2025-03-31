80d8e499204ca767685b414d53d4995ba02de409
Jake Rottier <jbrottier@wisc.edu>
2025-03-31 17:53:23 -0500

    Updated pre.sh

--- pre.sh
+++ pre.sh
@@ -8,6 +8,9 @@ mkdir -p "$dir/data"
 zip="$dir/data/nyc-taxi-dataset.zip"
 output="$dir/data"
 
+# This is actually going to be done manually since it is hard to do in a script since it needs keys and requirements
+# The data should be downloaoded to data/nyc-taxi-dataset.zip for the scripts to work
+
 # Download the dataset zip file **THIS MIGHT NOT WORK BECAUSE OF AUTHENTICATION AND API KEYS**
 curl -L -o "$zip" \
   https://www.kaggle.com/api/v1/datasets/download/microize/nyc-taxi-dataset
