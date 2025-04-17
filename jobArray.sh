1d0154f050972098c1a9202bb2e2941619b5cf40
Jake <jbrottier@wisc.edu>
2025-04-17 09:55:27 -0500

    Delete jobArray.sh

--- jobArray.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Setting the year based on the specified array
-year=$(($SLURM_ARRAY_TASK_ID + 2008))
-
-echo "Processing year $year..."
-
-# Procoess the specified year using the created python script
-python process_dataset.py $1 $year
